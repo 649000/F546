28cde0daa5363af0db4410c2f05b149e01f75aee
Nazri <C130071@e.ntu.edu.sg>
2016-02-29 00:53:33 +0800

    Preliminary Plot Charting for TRroute Path.

--- src/app/scripts/IPAddrDecoderService.js
+++ src/app/scripts/IPAddrDecoderService.js
@@ -23,6 +23,17 @@ var executionURL_FreeGeoIP = baseAPIURL_FreeGeoIP+':ip_address'
 var baseAPIURL_IPAPI = 'http://ip-api.com/json/'
 var executionURL_IPAPI = baseAPIURL_IPAPI+':ip_address'
 
+
+//9bf7a0d2242a9aefe51a62c64f512589791b09a1d47209f4e11fc6fd83ff4d81
+//http://www.ipinfodb.com/ip_location_api.php
+var baseAPIURL_IP_INFO_DB = 'http://api.ipinfodb.com/v3/ip-city/?key=9bf7a0d2242a9aefe51a62c64f512589791b09a1d47209f4e11fc6fd83ff4d81&format=json&ip='
+var executionURL_IP_INFO_DB = baseAPIURL_IP_INFO_DB +':ip_address'
+
+
+var baseAPIURL_GEOIP_NEKUDO='http://geoip.nekudo.com/api/'
+var executionURL_GEOIP_NEKUDO = baseAPIURL_GEOIP_NEKUDO+':ip_address'
+
+
 /**
  * JSON RECEIVED AS
  * Latitude: latitude
@@ -47,8 +58,31 @@ ipAddrDecodeServices.factory('IP_API', ['$resource', function($resource){
 
   return $resource(executionURL_IPAPI, {}, {
     decode: {method:'GET', params:{}, isArray:false}
+  });
 
+}]);
 
+/**
+ * JSON RECEIVED AS
+ * Latitude: latitude
+ * Longitude: longitude
+ */
+ipAddrDecodeServices.factory('IP_INFO_DB', ['$resource', function($resource){
+
+  return $resource(executionURL_IP_INFO_DB, {}, {
+    decode: {method:'GET', params:{}, isArray:false}
+  });
+
+}]);
+
+/**
+ * .location.latitude
+ * .location.longitude
+ */
+ipAddrDecodeServices.factory('GEOIP_NEKUDO', ['$resource', function($resource){
+
+  return $resource(executionURL_GEOIP_NEKUDO, {}, {
+    decode: {method:'GET', params:{}, isArray:false}
   });
 
 }]);
