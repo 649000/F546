63f77a634753c16741dc3858f5a01fa9216e62ba
Nazri <C130071@e.ntu.edu.sg>
2016-08-08 13:23:36 +0800

    Changed default layout of individual TR path to Circle layout

--- src/app/scripts/controllers/Traceroute/TracerouteCtrl.js
+++ src/app/scripts/controllers/Traceroute/TracerouteCtrl.js
@@ -1430,16 +1430,17 @@ angular.module('traceroute').controller('IndividualTracerouteGraphPanelCtrl', ['
 
 
     var layoutOptions = {
-      name: 'breadthfirst',
+      name: 'circle',
+
       fit: true, // whether to fit the viewport to the graph
-      directed: false, // whether the tree is directed downwards (or edges can point in any direction if false)
-      padding: 30, // padding on fit
-      circle: false, // put depths in concentric circles if true, put depths top down if false
-      spacingFactor: 1.75, // positive spacing factor, larger => more space between nodes (N.B. n/a if causes overlap)
+      padding: 30, // the padding on fit
       boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-      avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
-      roots: undefined, // the roots of the trees
-      maximalAdjustments: 0, // how many times to try to position the nodes in a maximal way (i.e. no backtracking)
+      avoidOverlap: true, // prevents node overlap, may overflow boundingBox and radius if not enough space
+      radius: undefined, // the radius of the circle
+      startAngle: 3 / 2 * Math.PI, // where nodes start in radians
+      sweep: undefined, // how many radians should be between the first and last node (defaults to full circle)
+      clockwise: true, // whether the layout should go clockwise (true) or counterclockwise/anticlockwise (false)
+      sort: undefined, // a sorting function to order the nodes; e.g. function(a, b){ return a.data('weight') - b.data('weight') }
       animate: false, // whether to transition the node positions
       animationDuration: 500, // duration of animation in ms if enabled
       animationEasing: undefined, // easing of animation if enabled
